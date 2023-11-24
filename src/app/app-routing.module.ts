a8fa955bf71a914d887b4f8d38ea22fc763105bf
gervegacastro <ger.vegacastro@gmail.com>
2023-11-24 16:17:22 -0300

    maquetado 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'detail/:id', component: DetailComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'dashboard' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    DetailComponent,
+    NavbarComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
